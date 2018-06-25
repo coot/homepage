e7ef9454a49a42f3a64f73a5b98466a9c4c41b4a
Marcin Szamotulski <profunctor@pm.me>
2018-06-25 10:40:44 +0200

    Disabled service worker

--- sw.js
+++ sw.js
@@ -85,72 +85,7 @@ workbox.routing.registerRoute(
   })
 )
 
-workbox.precaching.precacheAndRoute([
-  {
-    "url": "about.html",
-    "revision": "7fa96c418cc8a30825e978d241694540"
-  },
-  {
-    "url": "header.html",
-    "revision": "681d0b4c1c7cda091ce2c485b591a85c"
-  },
-  {
-    "url": "index.html",
-    "revision": "38968ba7a3270900ff08c6fe475fb920"
-  },
-  {
-    "url": "posts/adts-and-universal-algebra.html",
-    "revision": "54f58153bbdb1772b1eccbc65f06e0e7"
-  },
-  {
-    "url": "posts/free-monads.html",
-    "revision": "5ca8445f4c4c5ae302afabc6b836508b"
-  },
-  {
-    "url": "images/indie_hosters.svg",
-    "revision": "4f88c2f3ed4cd728ce40926259e9c99e"
-  },
-  {
-    "url": "images/marcinszamotulski-250.jpg",
-    "revision": "026773f54cf2287b5bd06a21bbfab74d"
-  },
-  {
-    "url": "images/marcinszamotulski-480.jpg",
-    "revision": "55cdddeea070a2520c5c8a8039fae87f"
-  },
-  {
-    "url": "images/vim_icon_20.png",
-    "revision": "bb22238b1625e6c10fc966ef30b6000a"
-  },
-  {
-    "url": "assets/index.js",
-    "revision": "f23dc1fb6ae887f9412bacdc97407c57"
-  },
-  {
-    "url": "assets/normalize.css",
-    "revision": "dca0089e5b1a1579441226245958e80f"
-  },
-  {
-    "url": "assets/print_style.css",
-    "revision": "34b729e90eadff3b019e2bead0ef4db3"
-  },
-  {
-    "url": "assets/script.js",
-    "revision": "118c50c800146611328cf17106aa8ba7"
-  },
-  {
-    "url": "assets/style.css",
-    "revision": "ccc0c1f2f8b33ab956a8b1ba32edddbd"
-  },
-  {
-    "url": "bower_components/html5shiv/dist/html5shiv.min.js",
-    "revision": "40bd440d29b3a9371b0c63fec41ee64f"
-  },
-  {
-    "url": "bower_components/html5shiv/dist/html5shiv-printshiv.min.js",
-    "revision": "9f03100ab5ce18e0049c25c6c4916802"
-  }
-]);
+workbox.precaching.precacheAndRoute([]);
 
 self.addEventListener('install', function(event) {
   self.skipWaiting();
