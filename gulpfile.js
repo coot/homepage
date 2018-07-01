1b91325c2715d156b5db5f761d2f15dff84b03e8
Marcin Szamotulski <profunctor@pm.me>
2018-07-01 23:34:08 +0200

    gulp build script

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+const fs = require("fs")
+const gulp = require("gulp")
+const nunjucksGulp = require("gulp-nunjucks")
+const nunjucks = require("nunjucks")
+const htmlmin = require("gulp-htmlmin")
+const cleanCSS = require("gulp-clean-css")
+const image = require("gulp-image")
+const workbox = require("workbox-build")
+
+function renderTemplate(path) {
+    nunjucks.render(path, {}, (err, res) => {
+        if (err != null) {
+            console.error(err)
+            return
+        }
+        fs.writeFileSync("dist/" + path, res);
+    })
+}
+
+let env = new nunjucks.Environment(new nunjucks.FileSystemLoader("templates"), { autoescape: false });
+
+gulp.task("html", () => {
+    return gulp.src([
+        "templates/index.html",
+        "templates/about.html",
+    ])
+        .pipe(nunjucksGulp.compile(
+            { wrapperClass: ""},
+            { env }
+        ))
+        // .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest("dist"))
+})
+
+gulp.task("html:posts", () => {
+    gulp.src([
+        "templates/posts/free-monads.html",
+        "templates/posts/peano-algebras-in-haskell.html"
+    ])
+        .pipe(nunjucksGulp.compile(
+            { wrapperClass: "post" },
+            { env }
+        ))
+        // .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest("dist/posts"))
+})
+
+gulp.task("css", () => {
+    return gulp.src("assets/*.css")
+        .pipe(cleanCSS())
+        .pipe(gulp.dest("dist/assets"))
+})
+
+gulp.task("images", () => {
+    return gulp.src("images/*.{png,jpg,svg}")
+        .pipe(image())
+        .pipe(gulp.dest("dist/images"))
+})
+
+gulp.task("js", () => {
+    return gulp.src("assets/*.js")
+        .pipe(gulp.dest("dist/assets"))
+})
+
+gulp.task("default", ["html", "html:posts", "images", "css", "js"], () => {
+    return workbox.injectManifest({
+        globDirectory: "./dist",
+        globPatterns: [
+            "images/*.{svg,jpg,png}",
+            "bower_components/html5shiv/dist/html5shiv.js",
+            "bower_components/html5shiv/dist/html5shiv-printshiv.js",
+        ],
+        swSrc: "./dev/sw.js",
+        swDest: "./dist/sw.js"
+    })
+})
--- workbox-config.js
+++
@@ -1,8 +0,0 @@
-exports.globDirectory = ".";
-exports.globPatterns = [
-    "images/*.{svg,jpg,png}",
-    "bower_components/html5shiv/dist/html5shiv.js",
-    "bower_components/html5shiv/dist/html5shiv-printshiv.js",
-  ],
-exports.swSrc = "./dev/sw.js"
-exports.swDest = "./sw.js"
