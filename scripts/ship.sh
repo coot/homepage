91070f3019e433cab0b6ebb18a1725685faedc13
Marcin Szamotulski <profunctor@pm.me>
2019-01-31 23:36:26 +0100

    Fixed the ship.sh script

--- scripts/ship.sh
+++ scripts/ship.sh
@@ -2,6 +2,6 @@
 
 tar -caf ./dist.tar.gz --exclude "*.un~" --exclude ".*.sw[pomnrst]" ./dist
 scp dist.tar.gz root@coot.me:/var/www/homepage
-ssh root@coot.me "tar -taf /var/www/homepage/dist.tar.gz > /dev/null"
+ssh root@coot.me "tar -C /var/www/homepage -xaf /var/www/homepage/dist.tar.gz"
 ssh root@coot.me "rm /var/www/homepage/dist.tar.gz"
 rm dist.tar.gz
