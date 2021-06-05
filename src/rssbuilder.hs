7fd22bf74d45aa8a2205bc843f6f85e65093e298
Marcin Szamotulski <profunctor@pm.me>
2021-06-05 18:43:51 +0200

    rss: added build date

--- src/rssbuilder.hs
+++ src/rssbuilder.hs
@@ -16,15 +16,18 @@ import           System.Exit
 import           Data.Aeson
 import qualified Data.ByteString.Lazy as BS
 import           Data.Text (Text)
+import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
+import qualified Data.Time as Time
 import           GHC.Generics (Generic (..))
 
 
 data Entry d = Entry {
-    eTitle :: Text,
-    eUrl   :: Text,
-    eDate  :: Text,
-    eData  :: d
+    eTitle     :: Text,
+    eUrl       :: Text,
+    eDate      :: Text,
+    eData      :: d,
+    eBuildDate :: Text
   }
   deriving (Generic, Functor)
 
@@ -34,17 +37,20 @@ instance FromJSON a => FromJSON (Entry a) where
             <*> v .: "url"
             <*> v .: "date"
             <*> v .: "data"
+            <*> pure mempty
 
 instance ToJSON a => ToJSON (Entry a) where
     toEncoding Entry { eTitle
                      , eUrl
                      , eDate
                      , eData
+                     , eBuildDate
                      } =
-      pairs $ "title" .= eTitle
-           <> "url"  .= eUrl
-           <> "date" .= eDate
-           <> "data" .= eData
+      pairs $ "title"         .= eTitle
+           <> "url"           .= eUrl
+           <> "date"          .= eDate
+           <> "data"          .= eData
+           <> "lastBuildDate" .= eBuildDate
 
 data Entries a = Entries {
     entries :: [a],
@@ -55,7 +61,7 @@ data Entries a = Entries {
 instance FromJSON a => FromJSON (Entries a) where
     parseJSON = withObject "Entries" $ \v ->
       Entries <$> v .: "entries"
-              <*> v .: "pubDate"
+              <*> pure mempty
 
 instance ToJSON a => ToJSON (Entries a) where
     toEncoding Entries { entries, pubDate } =
@@ -79,26 +85,30 @@ main = do
       Left err   -> putStrLn ("json parse error: " ++ err)
                  >> exitFailure
       Right feed -> return feed
+    t <- Time.getCurrentTime
+    let eBuildDate = Text.pack $ Time.formatTime Time.defaultTimeLocale Time.rfc822DateFormat t
 
     (entries' :: [Entry Text]) <-
-      traverse (\e -> updateEntry e <$> Text.readFile (eData e))
+      traverse (\e -> updateEntry e eBuildDate <$> Text.readFile (eData e))
                (entries feed)
     let feed' = Entries { entries = entries'
-                        , pubDate = pubDate feed
+                        , pubDate = eBuildDate
                         }
     case outputFile of
       "-" -> BS.putStr $ encode feed'
       _   -> encodeFile outputFile feed'
   where
-    updateEntry :: Entry String -> Text -> Entry Text
+    updateEntry :: Entry String -> Text -> Text -> Entry Text
     updateEntry Entry { eTitle
                       , eUrl
                       , eDate
                       }
+                eBuildDate
                 eData =
                 Entry { eTitle
                       , eUrl
                       , eDate
                       , eData
+                      , eBuildDate
                       }
 
